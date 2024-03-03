708c8c9d8ef3a76ca23454b1ec60526d9e9ece25
KosmachMariia <kosmach_2004@mail.ru>
2024-03-03 20:37:13 +0300

    Add TutorModel

---
+++ TutorService/src/Application/TutorService.Application.Models/TutorModel.cs
@@ -0,0 +1,11 @@
+namespace DefaultNamespace;
+
+public class TutorModel: UserModel
+{
+        private String _tutorId;
+        private String _userId;
+        private String[] _subjects;
+        private String _dateStartTeaching;
+        private String _place;
+
+}
