805580797b6ac1136d9cef7c271f318b0d234b82
Soudagh <fr.kristina@inbox.ru>
2024-03-15 22:44:19 +0300

    docker

--- src/Infrastructure/TutorService.Infrastructure.Persistence/Contexts/ApplicationDbContext.cs
+++ src/Infrastructure/TutorService.Infrastructure.Persistence/Contexts/ApplicationDbContext.cs
@@ -5,16 +5,24 @@ namespace TutorService.Infrastructure.Persistence.Contexts;
 
 public class ApplicationDbContext : DbContext
 {
-    public ApplicationDbContext() {}
+    public ApplicationDbContext() { }
+
     public ApplicationDbContext(DbContextOptions options) : base(options) { }
-    public required DbSet<UserModel> Users { get; set; }
-    public required DbSet<ExerciseModel> Exercises { get; set; }
-    public required DbSet<StudentModel> Students { get; set; }
-    public required DbSet<TaskThemeModel> TaskThemes { get; set; }
-    public required DbSet<ThemeModel> Themes { get; set; }
+
+    required public DbSet<UserModel> Users { get; set; }
+
+    required public DbSet<ExerciseModel> Exercises { get; set; }
+
+    required public DbSet<StudentModel> Students { get; set; }
+
+    required public DbSet<TaskThemeModel> TaskThemes { get; set; }
+
+    required public DbSet<ThemeModel> Themes { get; set; }
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+
         // Сюда добавлять различные конфигурации ваших файлов
         base.OnModelCreating(modelBuilder);
     }
